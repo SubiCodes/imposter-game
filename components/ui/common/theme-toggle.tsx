a8ef9b195c2477396a882191b4bb55226777fcc2
Rein Subisol <subisolreineerkim@gmail.com>
2026-01-03 18:19:01 +0800

    feat: Created the ThemeToggle component and cleared out main page

---
+++ components/ui/common/theme-toggle.tsx
@@ -0,0 +1,28 @@
+import { MoonStarIcon, StarIcon, SunIcon } from 'lucide-react-native';
+import React from 'react'
+import { Button } from '../button';
+import { useColorScheme } from 'nativewind';
+import { Icon } from '../icon';
+
+const ThemeToggle = () => {
+    const THEME_ICONS = {
+        light: SunIcon,
+        dark: MoonStarIcon,
+    };
+
+    function ThemeToggle() {
+        const { colorScheme, toggleColorScheme } = useColorScheme();
+
+        return (
+            <Button
+                onPressIn={toggleColorScheme}
+                size="icon"
+                variant="ghost"
+                className="ios:size-9 rounded-full web:mx-4">
+                <Icon as={THEME_ICONS[colorScheme ?? 'light']} className="size-5" />
+            </Button>
+        );
+    }
+}
+
+export default ThemeToggle
